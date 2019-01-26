3550a4664251fdecf62077130cf359177eb9eeec
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-01-26 11:32:02 +0530

    Remove test comments :+1:

--- utils/clearscreen.go
+++ utils/clearscreen.go
@@ -8,22 +8,22 @@ import (
 
 func ClearScreen() {
 
-	var clear = make(map[string]func()) //Initialize it
+	var clear = make(map[string]func())
     clear["linux"] = func() { 
-        cmd := exec.Command("clear") //Linux example, its tested
+        cmd := exec.Command("clear")
         cmd.Stdout = os.Stdout
         cmd.Run()
     }
     clear["windows"] = func() {
-        cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested 
+        cmd := exec.Command("cmd", "/c", "cls")
         cmd.Stdout = os.Stdout
         cmd.Run()
     }
 
-    value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
-    if ok { //if we defined a clear func for that platform:
-        value()  //we execute it
-    } else { //unsupported platform
+    value, ok := clear[runtime.GOOS]
+    if ok {
+        value()
+    } else {
         panic(" Something went wrong!!\n Could not clear the screen")
     }
 }
