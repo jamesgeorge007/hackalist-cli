06c28a4addd6ef747fba460c98ffcccc0ee762a7
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-01-26 12:33:25 +0530

    Minor bug fixes :construction:

--- cmd/hackathons.go
+++ cmd/hackathons.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"time"
 	"log"
+	"os"
+	"io/ioutil"
 
 	"github.com/fatih/color"
 	"github.com/briandowns/spinner"
@@ -16,51 +18,69 @@ import (
 
 const API_URL = "http://www.hackalist.org/api/1.0/2019/01.json"
 
-type Hackathon struct {
-	Title         string `json:"title"`
-	URL           string `json:"url"`
-	StartDate     string `json:"startDate"`
-	EndDate       string `json:"endDate"`
-	Year          string `json:"year"`
-	City          string `json:"city"`
-	Host          string `json:"host"`
-	Length        string `json:"length"`
-	Size          string `json:"size"`
-	Travel        string `json:"travel"`
-	Prize         string `json:"prize"`
-	HighSchoolers string `json:"highSchoolers"`
-	Cost          string `json:"cost"`
-	FacebookURL   string `json:"facebookURL"`
-	TwitterURL    string `json:"twitterURL"`
-	GooglePlusURL string `json:"googlePlusURL"`
-	Notes         string `json:"notes"`
+type Hackathons struct {
+	Months []struct {
+		Title         string `json:"title"`
+		URL           string `json:"url"`
+		StartDate     string `json:"startDate"`
+		EndDate       string `json:"endDate"`
+		Year          string `json:"year"`
+		City          string `json:"city"`
+		Host          string `json:"host"`
+		Length        string `json:"length"`
+		Size          string `json:"size"`
+		Travel        string `json:"travel"`
+		Prize         string `json:"prize"`
+		HighSchoolers string `json:"highSchoolers"`
+		Cost          string `json:"cost"`
+		FacebookURL   string `json:"facebookURL"`
+		TwitterURL    string `json:"twitterURL"`
+		GooglePlusURL string `json:"googlePlusURL"`
+		Notes         string `json:"notes"`
+	}
 }
 
 func listHackathons() {
 
 	spin := spinner.New(spinner.CharSets[14], 100*time.Millisecond) 
+	
 	utils.ClearScreen()
 	utils.ShowBanner()
 	color.Cyan(" \nFetching Data")
 	spin.Start()
 	time.Sleep(2 * time.Second)
 
-	var httpClient = &http.Client{Timeout: 10 * time.Second}
-	
-	resp, err := httpClient.Get(API_URL)
+    client := &http.Client{
+		Timeout: time.Duration(1 * time.Minute),
+	}
 
-    if err != nil {
-        log.Fatal(err)
-    }
+    resp, err := client.Get(API_URL)
 
-    defer resp.Body.Close()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    var hackathons Hackathon
-	data := json.NewDecoder(resp.Body).Decode(&hackathons)
+	defer resp.Body.Close()
 
-	spin.Stop()
+	var hackathon Hackathons
 
-	fmt.Println(data)
+	if resp.StatusCode == http.StatusOK {
+		// read response body
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		
+		err = json.Unmarshal(body, &hackathon)
+		if err != nil {
+			log.Println(err)
+		}
+
+		spin.Stop()
+
+		fmt.Println(len(hackathon.Months))
+	}
 }
 
 var hackathonCmd = &cobra.Command{
