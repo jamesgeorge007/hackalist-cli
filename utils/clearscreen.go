e04bfc4c5ec64e7940f12afd8f4bc295acff5ddc
Zach Peters <zpeters@gmail.com>
2020-10-01 20:38:25 -0500

    Added cat for clearning mac/darwin

--- utils/clearscreen.go
+++ utils/clearscreen.go
@@ -2,28 +2,33 @@ package utils
 
 import (
 	"os"
-	"runtime"
 	"os/exec"
+	"runtime"
 )
 
 func ClearScreen() {
 
 	var clear = make(map[string]func())
-    clear["linux"] = func() { 
-        cmd := exec.Command("clear")
-        cmd.Stdout = os.Stdout
-        cmd.Run()
-    }
-    clear["windows"] = func() {
-        cmd := exec.Command("cmd", "/c", "cls")
-        cmd.Stdout = os.Stdout
-        cmd.Run()
-    }
+	clear["linux"] = func() {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["darwin"] = func() {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
 
-    value, ok := clear[runtime.GOOS]
-    if ok {
-        value()
-    } else {
-        panic(" Something went wrong!!\n Could not clear the screen")
-    }
+	value, ok := clear[runtime.GOOS]
+	if ok {
+		value()
+	} else {
+		panic(" Something went wrong!!\n Could not clear the screen")
+	}
 }
